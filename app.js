6627fb283e1bbb79910b67e3f6fdc19473c19f6c
Kris <krzysztofkoz6@gmail.com>
2021-11-11 00:08:43 +0100

    Implement player choice function

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const choices = ["rock", "paper", "scissors"];
+const choices = { rock: "rock", paper: "paper", scissors: "scissors" };
 
 function getRandomChoice() {
   const maxIndexValue = choices.length - 1;
   const randomIndexValue = Math.floor(Math.random() * (maxIndexValue + 1));
 
   return choices[randomIndexValue];
+}
+
+function getPlayerChoice() {
+  let userInput = prompt("Choose by typing either rock, paper, or scissors").toLowerCase();
+
+  while (userInput.toLowerCase() in choices !== true) {
+    userInput = prompt("Incorrect input. Please type either rock, paper, or scissors");
+  }
+
+  return userInput;
 }
 
 function computerPlay() {

f354428349820d3f373c26a432270522ed7b1253
Kris <krzysztofkoz6@gmail.com>
2021-11-11 00:16:50 +0100

    Rename computerPlay function and choices variable

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const choices = { rock: "rock", paper: "paper", scissors: "scissors" };
+const validChoices = { rock: "rock", paper: "paper", scissors: "scissors" };
 
 function getRandomChoice() {
-  const maxIndexValue = choices.length - 1;
+  const maxIndexValue = validChoices.length - 1;
   const randomIndexValue = Math.floor(Math.random() * (maxIndexValue + 1));
 
-  return choices[randomIndexValue];
+  return validChoices[randomIndexValue];
 }
 
 function getPlayerChoice() {
   let userInput = prompt("Choose by typing either rock, paper, or scissors");
 
-  while (userInput?.toLowerCase() in choices !== true) {
+  while (userInput?.toLowerCase() in validChoices !== true) {
     userInput = prompt("Incorrect input. Please type either rock, paper, or scissors");
   }
 
   return userInput;
 }
 
-function computerPlay() {
+function getComputerChoice() {
   return getRandomChoice();
 }
